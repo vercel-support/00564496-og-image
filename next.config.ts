01467c8024abbea4149fd9cb0e746f2a38a1f8d9
Nick Vigier <nvigier46@gmail.com>
2025-06-17 13:58:40 +0100

    Add outputFileTracingIncludes for opengraph-image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    '/[locale]/opengraph-image': ['./src/assets/**/*'],
+  },
 };
 
 export default nextConfig;
